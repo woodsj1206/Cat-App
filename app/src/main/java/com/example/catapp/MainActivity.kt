c79dbbdcb77e4bbc1ee01b8846485f9400fd5da9
woodsj1206 <47829885+woodsj1206@users.noreply.github.com>
2024-04-10 22:22:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -0,0 +1,22 @@
+/*
+App Name: CatApp
+Author: woodsj1206 (https://github.com/woodsj1206)
+Description: A simple app that uses the CatAPI to display information about cats.
+Course: CIS 436
+Date Created: 4/10/24
+Last Modified: 4/10/24
+*/
+package com.example.catapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.catapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
